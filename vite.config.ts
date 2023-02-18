a65ed9b5acbb7b78a2a05b09d77fa324bc2a37d4
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-02-18 18:13:22 +0200

    Initialized Project

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import postcssImport from "postcss-import";
+import autoprefixer from "autoprefixer";
+import cssNano from "cssnano";
+import postcssPresetEnv from "postcss-preset-env";
+import postcssAdvancedVariables from "postcss-advanced-variables";
+import postcssExtendRule from "postcss-extend-rule";
+import postcssNested from "postcss-nested";
+
+export default defineConfig({
+    css: {
+        postcss: {
+            plugins: [
+                postcssImport,
+                cssNano,
+                autoprefixer,
+                postcssPresetEnv({ stage: 1 }),
+                postcssNested,
+                postcssAdvancedVariables,
+                postcssExtendRule,
+            ],
+        },
+    },
+});
